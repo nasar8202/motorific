13099ec4dec0311c46dd5d5ececec17e69d9548c
nasar8202 <105274484+nasar8202@users.noreply.github.com>
2023-02-10 11:28:25 +0500

    add subscr

---
+++ app/Http/Controllers/frontend/NewsletterSubscribers.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\frontend;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\NewsletterSubscriber;
+
+class NewsletterSubscribers extends Controller
+{
+    public function addSubscriberEmail(Request $request)
+    {
+        if($request->ajax()){
+            $data = $request->all();
+            
+            $subcriberEmailCheck = NewsletterSubscriber::where('email',$data['subscriber_email'])->count();
+            if($subcriberEmailCheck>0){
+                return "exists";
+            }else{
+               
+                $subscribe = new NewsletterSubscriber();
+                $subscribe->email = $data['subscriber_email'];
+                $subscribe->status = 1;
+                $subscribe->email = $data['subscriber_email'];
+                $subscribe->save();
+                return "inserted";
+
+            }
+        }
+    }
+}
