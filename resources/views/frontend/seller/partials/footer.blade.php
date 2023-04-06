095014b891f75d85efff74c1351dde1b461b0935
UmerMansoor100 <umer.mansoor@oip.com.pk>
2023-04-05 22:25:47 -0400

    new css

--- resources/views/frontend/seller/partials/footer.blade.php
+++ resources/views/frontend/seller/partials/footer.blade.php
@@ -1,7 +1,7 @@
 
  <!-- FOOTER -->
 
- <section class="sec-8">
+ <section class="sec-8 footer-sec">
     <div class="container-1151">
         <div class="main-footer">
             <div class="footer-sub about">
@@ -79,15 +79,20 @@
             <div class="footer-sub contact-2">
                <a href="{{ route('index') }}"> <img src="{{ URL::asset('frontend/seller/assets/image/logo.png')}}" alt=""></a>
                 <div class="d-flex mt-4 mb-4">
-                    <a href="#">
-                        <div class="footer-icon-bg"><i class="fa-brands fa-twitter"></i></div>
-                    </a>
-                    <a href="https://www.facebook.com/Motorific-100480046330830">
-                        <div class="footer-icon-bg"><i class="fa-brands fa-facebook-f"></i></div>
-                    </a>
-                    <a href="https://www.instagram.com/motorific_1/">
-                        <div class="footer-icon-bg"><i class="fa-brands fa-instagram"></i></div>
-                    </a>
+                    <ul class="footer-social">
+                        <li><a href="#"><i class="fa-brands fa-twitter"></i></a></li>
+                        <li><a href="https://www.facebook.com/Motorific-100480046330830"><i class="fa-brands fa-facebook-f"></i></a></li>
+                        <li><a href="https://www.instagram.com/motorific_1/"><i class="fa-brands fa-instagram"></i></a></li>
+                    </ul>
+                    <!--<a href="#">-->
+                    <!--    <div class="footer-icon-bg"><i class="fa-brands fa-twitter"></i></div>-->
+                    <!--</a>-->
+                    <!--<a href="https://www.facebook.com/Motorific-100480046330830">-->
+                    <!--    <div class="footer-icon-bg"><i class="fa-brands fa-facebook-f"></i></div>-->
+                    <!--</a>-->
+                    <!--<a href="https://www.instagram.com/motorific_1/">-->
+                    <!--    <div class="footer-icon-bg"><i class="fa-brands fa-instagram"></i></div>-->
+                    <!--</a>-->
                 </div>
                 <h4>© Motorific Online Ltd. <?php echo date("Y"); ?></h4>
             </div>
