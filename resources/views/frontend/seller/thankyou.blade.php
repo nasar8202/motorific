404fe09a1f6b6fc28eabf351b2c64d8139bdbb15
Nasar Ullah <nasar.ullah@binaryit.co>
2023-04-18 12:48:00 +0500

    as

--- resources/views/frontend/seller/thankyou.blade.php
+++ resources/views/frontend/seller/thankyou.blade.php
@@ -8,7 +8,7 @@
 @section('logoMain','https://motorific.co.uk/frontend/seller/assets/image/logo.png')
 @section('ContainerHeader','container-1600 d-flex justify-content-between pt-4')
 
-
+<!-- You will receive your tailored offer shortly via text or email. -->
 
 <section class="thanks">
     <div class="container-1151">
@@ -16,7 +16,7 @@
             <span class="icon"><i class="fas fa-check-circle"></i></span>
             <h2>Thank You For Choosing Us</h2>
             {{-- <h6>Your Vehicle Has Been Submited. Wait For Admin Approvel</h6> --}}
-            <p>Your Advert is added  While you wait for admin approval, please call us if you need further assistance 07888185014. You will receive your tailored offer shortly via text or email.
+            <p>Your Advert is added  While you wait for admin approval, please call us if you need further assistance 07888185014. 
                 <a href="tel:07888185014"><i class="fa-solid fa-phone"></i> 07888185014.</a>
                 <br>
                 <a href="{{route('acceptedVehicles')}}">Check My Vehicle</a>
