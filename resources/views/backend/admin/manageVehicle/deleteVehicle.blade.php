85758fd4b75ff322a27bc84ef18d6b98e4b6cd20
Nasar Ullah <nasar.ullah@binaryit.co>
2023-04-21 15:27:04 +0500

    changes deployed by hannan

--- resources/views/backend/admin/manageVehicle/deleteVehicle.blade.php
+++ resources/views/backend/admin/manageVehicle/deleteVehicle.blade.php
@@ -82,7 +82,9 @@
                             <td class=""><span class="badge badge-success"> Buy It Now </span></td>
                             @endif
                             <td>
+                                @if(isset($vehicle->VehicleImage))
                                 <img src="{{ asset('/vehicles/vehicles_images/'.$vehicle->VehicleImage->front) }}" width="100" height="100">
+                                @endif
                             </td>
                             
                             <td>
